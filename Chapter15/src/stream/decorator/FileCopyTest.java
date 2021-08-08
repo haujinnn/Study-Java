0f7a0f089d85a57ba011623e32bc6dbbfb7228e6
haujin <uj0110@g.skku.edu>
2021-08-08 10:26:49 +0900

    week5

---
+++ Chapter15/src/stream/decorator/FileCopyTest.java
@@ -0,0 +1,25 @@
+package stream.decorator;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyTest {
+
+	public static void main(String[] args) {
+		long milliseconds = 0;
+
+		try( FileInputStream fis = new FileInputStream("a.exe");
+				FileOutputStream fos = new FileOutputStream("b.exe")){
+			milliseconds = System.currentTimeMillis();
+			int i;
+			while((i=fis.read())!=-1) {
+				fos.write(i);
+			}
+			milliseconds = System.currentTimeMillis() - milliseconds;
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+		System.out.println();
+	}
+}
